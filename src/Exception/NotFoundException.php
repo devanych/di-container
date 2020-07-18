0c21f685cf1bca2111af50eb289afa364f06e785
devanych <mail@devanych.ru>
2020-07-18 20:10:18 +0300

    Change qualifier to import in NotFoundException

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -2,8 +2,9 @@
 
 namespace Devanych\Di\Exception;
 
+use InvalidArgumentException;
 use Psr\Container\NotFoundExceptionInterface;
 
-class NotFoundException extends \InvalidArgumentException implements NotFoundExceptionInterface
+class NotFoundException extends InvalidArgumentException implements NotFoundExceptionInterface
 {
 }
