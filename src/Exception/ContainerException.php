516ca7d0e57b79f28fa9b6715a2adb73a6d34359
devanych <mail@devanych.ru>
2020-07-18 20:08:27 +0300

    Change qualifier to import in ContainerException

--- src/Exception/ContainerException.php
+++ src/Exception/ContainerException.php
@@ -2,8 +2,9 @@
 
 namespace Devanych\Di\Exception;
 
+use LogicException;
 use Psr\Container\ContainerExceptionInterface;
 
-class ContainerException extends \LogicException implements ContainerExceptionInterface
+class ContainerException extends LogicException implements ContainerExceptionInterface
 {
 }
