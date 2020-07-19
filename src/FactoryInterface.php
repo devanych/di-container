9a9337def2249892716663f7b6a1f0e3cf865150
devanych <mail@devanych.ru>
2020-07-19 14:54:37 +0300

    Add description to phpDoc in FactoryInterface

--- src/FactoryInterface.php
+++ src/FactoryInterface.php
@@ -9,6 +9,45 @@ use Psr\Container\ContainerInterface;
 interface FactoryInterface
 {
     /**
+     * Creates a new object using passed container that implements `Psr\Container\ContainerInterface`.
+     *
+     * A new object is created from previously defined dependencies or by using container autowiring.
+     *
+     * Example of use:
+     *
+     * ```php
+     * // Example of an ApplicationFactory test class:
+     * final class ApplicationFactory implements \Devanych\Di\FactoryInterface
+     * {
+     *     public ?string $environment;
+     *
+     *     public function __construct(string $environment = null)
+     *     {
+     *         $this->environment = $environment;
+     *     }
+     *
+     *     public function create(ContainerInterface $container): object
+     *     {
+     *         return new Application(
+     *             $container->get(Router::class),
+     *             $container->get(EmitterInterface::class),
+     *             $this->environment ?? $container->get('environment'),
+     *         );
+     *     }
+     * }
+     *
+     * // Example of setting dependencies:
+     * $container = new \Devanych\Di\Container([
+     *     'environment' => 'development',
+     *     Application::class => ApplicationFactory::class,
+     *     Router::class => RouterFactory::class,
+     *     EmitterInterface::class => EmitterFactory::class,
+     * ]);
+     *
+     * // Creating an Application instance:
+     * $application = $container->get(Application::class);
+     * ```
+     *
      * @param ContainerInterface $container
      * @return object
      */
